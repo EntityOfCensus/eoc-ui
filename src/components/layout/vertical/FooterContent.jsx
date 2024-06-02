b08368ae3cc4572c77439c968f14ea4f3c009c52
Octavian Stirbei <octavianstirbei@gmail.com>
2024-06-03 01:42:40 +0300

    footer content

--- src/components/layout/vertical/FooterContent.jsx
+++ src/components/layout/vertical/FooterContent.jsx
@@ -32,11 +32,11 @@ const FooterContent = () => {
         <span className='text-textSecondary'>{`© ${new Date().getFullYear()}, Made with `}</span>
         <span>{`❤️`}</span>
         <span className='text-textSecondary'>{` by `}</span>
-        <Link href='https://pixinvent.com' target='_blank' className='text-primary uppercase'>
-          Pixinvent
+        <Link href='https://entityofcode.com/' target='_blank' className='text-primary uppercase'>
+          Entity Of Code
         </Link>
       </p>
-      {!isBreakpointReached && (
+      {/* {!isBreakpointReached && (
         <div className='flex items-center gap-4'>
           <Link href='https://themeforest.net/licenses/standard' target='_blank' className='text-primary'>
             License
@@ -55,7 +55,7 @@ const FooterContent = () => {
             Support
           </Link>
         </div>
-      )}
+      )} */}
     </div>
   )
 }
