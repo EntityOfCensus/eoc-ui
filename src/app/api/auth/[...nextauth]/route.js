b77f3a156628629b132f44908e22655ea2616596
Octavian Stirbei <octavianstirbei@gmail.com>
2024-06-13 00:35:26 +0300

    cloudflare pages wip

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,4 +6,6 @@ import { authOptions } from '@/libs/auth'
 
 const handler = NextAuth(authOptions)
 
+export const runtime = 'edge' // 'nodejs' (default) | 'edge'
+
 export { handler as GET, handler as POST }
